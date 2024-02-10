99b6177e66372591ab7555723db83a8991be1213
Ravi-takhi <ravitakhi@ymail.com>
2024-02-10 01:26:39 -0800

    Correct class names to comply with BEM standard

--- design-system.js
+++ design-system.js
@@ -1,15 +1,15 @@
 function toggleNavbar(isVisible) {
   let sideNavbar = document.getElementById("side-navbar");
-  let navbarToggle = document.getElementById("navbar-toggle");
+  
   if (isVisible) {
-    document.getElementById("navbar-open").style.display = "none";
-    document.getElementById("navbar-close").style.display = "flex";
+    document.getElementById("sidebarShow").style.display = "none";
+    document.getElementById("sidebarHide").style.display = "flex";
     sideNavbar.style.display = "flex";
-    navbarToggle.classList.add("active");
+    sideNavbar.classList.add("active");
   } else {
-    document.getElementById("navbar-open").style.display = "flex";
-    document.getElementById("navbar-close").style.display = "none";
+    document.getElementById("sidebarShow").style.display = "flex";
+    document.getElementById("sidebarHide").style.display = "none";
     sideNavbar.style.display = "none";
-    navbarToggle.classList.remove("active");
+    sideNavbar.classList.remove("active");
   }
 }

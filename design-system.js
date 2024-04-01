7ac59abdb6dc07bdcbbeb4d00692fe702d546e1a
Ravi-takhi <ravitakhi@ymail.com>
2024-03-31 18:50:50 -0700

    Add  dropdown to menu bar

--- design-system.js
+++ design-system.js
@@ -1,12 +1,15 @@
 function toggleNavbar(isVisible) {
   let sideNavbar = document.getElementById("side-navbar");
+  var dropdownMenu = document.querySelector('.on-nav__dropdown');
   
   if (isVisible) {
+    dropdownMenu.style.display = 'block';
     document.getElementById("sidebarShow").style.display = "none";
     document.getElementById("sidebarHide").style.display = "flex";
     sideNavbar.style.display = "flex";
     sideNavbar.classList.add("active");
   } else {
+    dropdownMenu.style.display = 'none';
     document.getElementById("sidebarShow").style.display = "flex";
     document.getElementById("sidebarHide").style.display = "none";
     sideNavbar.style.display = "none";

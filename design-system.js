6b2ce882af54a1ef6f5dd75a10457fb5ae4ac2d8
Ravi-takhi <ravitakhi@ymail.com>
2024-04-16 23:50:11 -0700

    Add testimonial

--- design-system.js
+++ design-system.js
@@ -88,5 +88,38 @@ document.addEventListener("DOMContentLoaded", function() {
   showTestimonial();
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  let counter = 1;
+showTestimonials(counter);
 
+document.querySelector(".on-testimonials__prev").addEventListener("click", () => {
+    computeTestimonials(-1)
+})
 
+document.querySelector(".on-testimonials__next").addEventListener("click", () => {
+    computeTestimonials(1)
+})
+
+function computeTestimonials(t) {
+    showTestimonials((counter += t))
+}
+
+function showTestimonials(n) {
+  let i;
+  let testimonials = document.getElementsByClassName("on-testimonial");
+
+  if (n > testimonials.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = testimonials.length;
+  }
+
+  for( i = 0; i < testimonials.length; i++){
+      testimonials[i].style.display = "none"
+  }
+
+  testimonials[counter - 1].style.display = "flex"
+}
+
+});

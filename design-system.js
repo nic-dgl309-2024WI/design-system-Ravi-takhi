698cc44d644ae6f36a2116010cee8eb7de200959
Ravi-takhi <ravitakhi@ymail.com>
2024-04-17 21:05:31 -0700

    Add faq's and contact us section

--- design-system.js
+++ design-system.js
@@ -123,3 +123,19 @@ function showTestimonials(n) {
 }
 
 });
+document.addEventListener("DOMContentLoaded", function() {
+var acc = document.getElementsByClassName('on-faqs__item');
+var i;
+
+for (i =0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        }else{
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+});
